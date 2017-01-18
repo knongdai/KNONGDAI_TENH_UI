7fc524cfd785a8aa336db93c4d9852642069ccf5
Phearun Rath <rathphearun168@gmail.com>
2017-01-18 11:13:54 +0700

    update pom and property

--- src/main/webapp/resources/static/javascript/configuration.js
+++ src/main/webapp/resources/static/javascript/configuration.js
@@ -5,7 +5,7 @@
 //var URL_CLIENT 	= "http://localhost:8181/rest/";
 //var URL_API 	= "http://localhost:9999/api/";
 
-var API_PORT = 1302;
+var API_PORT = 9999;
 
 var URL_CLIENT = location.protocol + "//" + location.host + "/rest/";
 var URL_API    = location.protocol + "//" + location.hostname + ":" + API_PORT + "/api/";
